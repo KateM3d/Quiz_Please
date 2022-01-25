a19ab4b3c22faad7e1d73615ca467980192f7733
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-25 17:43:13 -0500

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Card from "./components/Card";
 import Question from "./components/Question";
 import NotFound from "./components/404";
@@ -6,9 +7,22 @@ import "./App.scss";
 export default function App() {
   return (
     <>
-      <NotFound />
-      {/* <Question /> */}
-      {/* <Card /> */}
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <h2>hello</h2>
+          </Route>
+          <Route exact path="/categories">
+            <Card />
+          </Route>
+          <Route exact path="/question">
+            <Question />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
