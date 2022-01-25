5de4c5563404925b16b1bc5c0897cc585f6684ec
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-25 17:16:36 -0500

    created 404 page

---
+++ src/components/404.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="container_category">
+      <p className="notFound">404</p>
+      <p className="notFound_text">☹️</p>
+      <p className="notFound_text">Page not found</p>
+      <br />
+    </div>
+  );
+}
