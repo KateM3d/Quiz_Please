767d558a244efb785fda7d4a85bc6fa6d13123f8
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-25 17:01:29 -0500

    added structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+import Question from "./components/Question";
+import "./App.scss";
+
+export default function App() {
+  return (
+    <>
+      <Question />
+      {/* <Card /> */}
+    </>
+  );
+}
