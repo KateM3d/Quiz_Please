5db075e23a962f6c4cd4e9568be17a3ff615aecd
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-27 18:03:21 -0500

    fixed correct rendering of the options when there is only 2 options not 4

--- src/components/CardInner.jsx
+++ src/components/CardInner.jsx
@@ -1,39 +1,41 @@
-import { APIContext } from "../context/ApiContext";
-
 export default function CardInner(props) {
   const shuffle = (opt) => {
     console.log(opt);
   };
   console.log(shuffle(props.options));
 
-  console.log(props);
+  console.log(props.options[2]);
   return (
     <div className="container_question">
       <h2 className="container_question_text">{props.categoryQuestion}</h2>
       <div className="container_question_answers">
         <p
           className="container_question_answers_option"
-          options={props.options}
+          options={props.options[0]}
         >
-          {props.options}
+          {props.options[0]}
         </p>
         <p
           className="container_question_answers_option"
-          options={props.options}
+          options={props.options[1]}
         >
-          {props.options}
+          {props.options[1]}
         </p>
         <p
-          className="container_question_answers_option"
-          options={props.options}
+          className={`container_question_answers_option ${
+            props.options[2] === undefined && "none"
+          }`}
+          options={props.options[2]}
         >
-          {props.options}
+          {props.options[2]}
         </p>
         <p
-          className="container_question_answers_option"
-          options={props.options}
+          className={`container_question_answers_option ${
+            props.options[2] === undefined && "none"
+          }`}
+          options={props.options[3]}
         >
-          {props.options}
+          {props.options[3]}
         </p>
       </div>
     </div>
