cbc0f51eb4693c2d1c2e1e714bf75fe89649eb82
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-12 13:55:52 -0500

    in progress shuffling answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react";
-import { useState, useContext } from "react";
+import { useState, useContext, useMemo } from "react";
 import { APIContext } from "../context/ApiContext";
 
 export default function Question() {
   const { getQuestionsData, categoryQuestion, options, correct } =
     useContext(APIContext);
   const [count, setCount] = useState(1);
-
   const [questionCount, setQuestionCount] = useState(0);
   const [score, setScore] = useState(0);
+
+  const shuffledOptions = () => {
+    let ind = [1, 2, 3, 4];
+    let i = 0;
+    let j = 0;
+    let temp = 0;
+    for (i = ind.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = ind[i];
+      ind[i] = ind[j];
+      ind[j] = temp;
+    }
+    return ind;
+  };
 
   const handleCountIncrease = () => {
     if (count === 10) {
@@ -19,6 +31,7 @@ export default function Question() {
     } else {
       setCount((prevCount) => prevCount + 1);
       setQuestionCount((prevCount) => prevCount + 1);
+      shuffledOptions();
     }
   };
   const selectedArray = [...options];
@@ -37,10 +50,12 @@ export default function Question() {
           {categoryQuestion[questionCount]}
         </h2>
         <div className="container_question_answers">
-          {selectedArray[questionCount]?.map((answer) => {
+          {selectedArray[questionCount]?.map((answer, i) => {
+            shuffledOptions();
+
             return (
               <p
-                key={answer}
+                key={i}
                 onClick={() => handleAnswerCheck({ answer })}
                 className="container_question_answers_option"
               >
