a1b0904c50b59dc0545c81f89a06bcc811c78742
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-11 19:04:37 -0500

    working on score

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,38 +1,33 @@
+import { useEffect } from "react";
 import { useState, useContext } from "react";
 import { APIContext } from "../context/ApiContext";
 
-export default function Question({}) {
-  const { categoryQuestion, options, correct } = useContext(APIContext);
+export default function Question() {
+  const { getQuestionsData, categoryQuestion, options, correct } =
+    useContext(APIContext);
   const [count, setCount] = useState(1);
-  const [disabled, setDisabled] = useState();
+
   const [questionCount, setQuestionCount] = useState(0);
   const [score, setScore] = useState(0);
 
   const handleCountIncrease = () => {
     if (count === 10) {
-      setDisabled("disabled");
       setCount(10);
+      setScore(score);
+
+      alert(`Your score is ${score}/10`);
     } else {
       setCount((prevCount) => prevCount + 1);
       setQuestionCount((prevCount) => prevCount + 1);
-      // if (correct === selected) {
-      setScore((prevScore) => prevScore + 1);
-      // }
     }
   };
   const selectedArray = [...options];
 
   const handleAnswerCheck = ({ answer }) => {
-    console.log(correct);
     const selectedAnswer = { answer };
-    console.log(selectedAnswer);
-    console.log(selectedArray[questionCount]);
-    const answerCheck = correct.includes(selectedAnswer.answer);
-    console.log(answerCheck);
-    if (answerCheck) {
-      const correctSelected = [selectedAnswer.answer];
-      console.log(correctSelected);
-    }
+    const answerCheck = correct.includes(selectedAnswer?.answer);
+    answerCheck && setScore((prevScore) => prevScore + 1);
+    handleCountIncrease();
   };
   return (
     <>
@@ -56,13 +51,6 @@ export default function Question({}) {
         </div>
       </div>
       <div className="container_question">
-        <button
-          className={`btn ${disabled === "disabled" ? "disabled" : ""}`}
-          disabled={disabled}
-          onClick={handleCountIncrease}
-        >
-          Next
-        </button>
         <h2 className="container_question_points">Score: {score}</h2>
       </div>
     </>
